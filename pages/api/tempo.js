85bf35e1e0d2e0edb55fe5ee70e2ba4011503153
Adoglésio Gomes <adoglesiogomes18@gmail.com>
2022-04-23 19:30:13 -0300

    usa variavel de ambiente

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,7 +1,8 @@
 async function tempo(request, response) {
+    const apiScret = process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date();
 
-    const subscribersResponse = await fetch("https://api.convertkit.com/v3/subscribers?api_secret=%3Cyour_secret_api_key%3E&from=2016-02-01&to=2015-02-28")
+    const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
     const subscribersResponseJson = await subscribersResponse.json();
     const inscritos = subscribersResponseJson.total_subscribers;
     response.json({
