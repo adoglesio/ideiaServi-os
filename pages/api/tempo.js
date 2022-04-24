52880d6004867e02e0684df3da32a3deab17ecf4
Adoglésio Gomes <adoglesiogomes18@gmail.com>
2022-04-23 21:14:11 -0300

    usa variavel de ambiente

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -5,6 +5,9 @@ async function tempo(request, response) {
     const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
     const subscribersResponseJson = await subscribersResponse.json();
     const inscritos = subscribersResponseJson.total_subscribers;
+
+    response.setHeader('Cache-Control', 's-maxege=10, stale-while-revalidate');
+
     response.json({
         date: dynamicDate.toGMTString(),
         inscritos: inscritos
